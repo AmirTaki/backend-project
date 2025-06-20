a916c2ce5e5dcb5d49158b2099a1effd53272691
amir <amirtaki623@gmail.com>
2025-06-20 20:27:45 +0330

    scroller grid img  js at project

---
+++ src/script/main.js
@@ -0,0 +1,20 @@
+/* ==================================================  CLASSIFIEER CONTAINER : GRID IMAGE  =========================================================== */
+let iconLeftScrollImg = document.querySelector(".iconLeftScrollImg")
+let iconRigthtScrollImg = document.querySelector(".iconRigthtScrollImg")
+let containerScroll = document.querySelector(".containerScroll")
+
+// CONTAINER SCROLL IMAGE WHELL
+containerScroll.addEventListener("wheel",(e)=>{
+    e.preventDefault()
+    containerScroll.scrollLeft += e.deltaY;
+})
+// CONTAINER SCROLL IMAGE CLICK LEFT
+iconLeftScrollImg.addEventListener("click",(e)=>{
+    containerScroll.style.scrollBehavior = "smooth";
+    containerScroll.scrollLeft -= 300
+})
+// CONTAINER SCROLL IMAGE CLICK RIGHT
+iconRigthtScrollImg.addEventListener("click",(e)=>{
+    containerScroll.style.scrollBehavior = "smooth";
+    containerScroll.scrollLeft += 300
+})
